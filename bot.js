dd822a1f04028aec46a608af21a23af97b999c4a
tyejae <tyejae@gmail.com>
2020-10-09 07:37:24 -0700

    simplifying

--- bot.js
+++ bot.js
@@ -11,49 +11,21 @@ bot.on("ready", function() {
 bot.on('message', message => {
     if (message.content.charAt(0) === '~') {
         message.delete(1000).catch();
+        let description = "Commands need to be run in <#697913862095241296>";
+        let title = "Wrong Channel";
         if (message.channel.id === '744913491999064294') {
-            return message.reply({
-                "embed": {
-                    "url": "http://discord.gg/",
-                    "description":"Los comandos deben ejecutarse en <#697913862095241296>",
-                    "title":"Canal Equivocado",
-                    "thumbnail":
-                    {
-                        "url":"https://msf.gg/exclamation-triangle-solid.png"
-                    },
-                    "color":14733329,
-                    "footer":
-                    {
-                        "icon_url":"https://msf.gg/msfgg-bot-36x36.png",
-                        "text":"MSF.gg Bot • https://msf.gg/bot • tyejae#2513"
-                    }
-                }
-            }).then(r => r.delete(30000)).catch();
+            description = "Los comandos deben ejecutarse en <#697913862095241296>";
+            title = "Canal Equivocado";
         }
         if (message.channel.id === '725423289903611904') {
-            return message.reply({
-                "embed": {
-                    "url": "http://discord.gg/",
-                    "description":"Les commandes doivent être exécutées <#697913862095241296>",
-                    "title":"Mauvaise Chaîne",
-                    "thumbnail":
-                    {
-                        "url":"https://msf.gg/exclamation-triangle-solid.png"
-                    },
-                    "color":14733329,
-                    "footer":
-                    {
-                        "icon_url":"https://msf.gg/msfgg-bot-36x36.png",
-                        "text":"MSF.gg Bot • https://msf.gg/bot • tyejae#2513"
-                    }
-                }
-            }).then(r => r.delete(30000)).catch();
+            description = "Les commandes doivent être exécutées <#697913862095241296>";
+            title = "Mauvaise Chaîne";
         }
         return message.reply({
             "embed": {
                 "url": "http://discord.gg/",
-                "description":"Commands need to be run in <#697913862095241296>",
-                "title":"Wrong Channel",
+                "description": description,
+                "title": title,
                 "thumbnail":
                 {
                     "url":"https://msf.gg/exclamation-triangle-solid.png"
